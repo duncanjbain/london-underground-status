5f995e72883289c920529caba2147e962b1c9516
Duncan Bain <duncan@bain.io>
2020-02-20 21:15:02 +0000

    disrupted lines first in list, then normal lines

--- script.js
+++ script.js
@@ -1,41 +1,80 @@
-const TFL_API_URL ="https://api.tfl.gov.uk/line/mode/tube,overground,dlr,tflrail/status";
+const TFL_API_URL =
+  "https://api.tfl.gov.uk/line/mode/tube,overground,dlr,tflrail/status";
 
 async function getTflStatus(apiURL) {
-    let response = await fetch(apiURL);
-    return await response.json();
+  let response = await fetch(apiURL);
+  return await response.json();
 }
 
 function showTflStatus() {
-getTflStatus(TFL_API_URL).then((data) => {
+  getTflStatus(TFL_API_URL).then(data => {
     console.log(data);
-    data.forEach(line => {
-        let lineStatusOverallContainer = document.createElement("div");
-        lineStatusOverallContainer.className =
-            "flex flex-row justify-between p-1 sm:p-2 border-b border-gray";
-        lineStatusOverallContainer.id = line.id;
 
-        lineStatusOverallContainer.classList.add(line.id);
-
-        let lineNameContainer = document.createElement("p");
-        let lineName = document.createTextNode(line.name);
-        lineNameContainer.appendChild(lineName);
-
-        let lineStatusContainer = document.createElement("p");
-        let lineStatus = document.createTextNode(
-            line.lineStatuses[0].statusSeverityDescription
-        );
-        lineStatusContainer.appendChild(lineStatus);
-
-        line.lineStatuses[0].statusSeverityDescription!== 'Good Service' ?  
-            lineStatusContainer.classList.add('font-bold') : lineStatusContainer.classList.add('font-normal')
-
-        lineStatusOverallContainer.append(lineNameContainer, lineStatusContainer);
-
-        document.getElementById("status").appendChild(lineStatusOverallContainer);
+    let nonNormalLinesOnly = data.filter(function(lineInfo) {
+      return lineInfo.lineStatuses[0].statusSeverity !== 10;
     });
-})
+
+    let normalLinesOnly = data.filter(function(lineInfo) {
+      return lineInfo.lineStatuses[0].statusSeverity === 10;
+    });
+
+    console.log(nonNormalLinesOnly);
+    nonNormalLinesOnly.forEach(line => {
+      let lineStatusOverallContainer = document.createElement("div");
+      lineStatusOverallContainer.className =
+        "flex flex-row justify-between p-1 sm:p-2 border-b border-gray";
+      lineStatusOverallContainer.id = line.id;
+
+      lineStatusOverallContainer.classList.add(line.id);
+
+      let lineNameContainer = document.createElement("p");
+      let lineName = document.createTextNode(line.name);
+      lineNameContainer.appendChild(lineName);
+
+      let lineStatusContainer = document.createElement("p");
+      let lineStatus = document.createTextNode(
+        line.lineStatuses[0].statusSeverityDescription
+      );
+      lineStatusContainer.appendChild(lineStatus);
+
+      line.lineStatuses[0].statusSeverityDescription !== "Good Service"
+        ? lineStatusContainer.classList.add("font-bold")
+        : lineStatusContainer.classList.add("font-normal");
+
+      lineStatusOverallContainer.append(lineNameContainer, lineStatusContainer);
+
+      document.getElementById("status").appendChild(lineStatusOverallContainer);
+    });
+
+    normalLinesOnly.forEach(line => {
+      let lineStatusOverallContainer = document.createElement("div");
+      lineStatusOverallContainer.className =
+        "flex flex-row justify-between p-1 sm:p-2 border-b border-gray";
+      lineStatusOverallContainer.id = line.id;
+
+      lineStatusOverallContainer.classList.add(line.id);
+
+      let lineNameContainer = document.createElement("p");
+      let lineName = document.createTextNode(line.name);
+      lineNameContainer.appendChild(lineName);
+
+      let lineStatusContainer = document.createElement("p");
+      let lineStatus = document.createTextNode(
+        line.lineStatuses[0].statusSeverityDescription
+      );
+      lineStatusContainer.appendChild(lineStatus);
+
+      line.lineStatuses[0].statusSeverityDescription !== "Good Service"
+        ? lineStatusContainer.classList.add("font-bold")
+        : lineStatusContainer.classList.add("font-normal");
+
+      lineStatusOverallContainer.append(lineNameContainer, lineStatusContainer);
+
+      document.getElementById("status").appendChild(lineStatusOverallContainer);
+    });
+  });
 }
 
-document.addEventListener('DOMContentLoaded', event => {
-    showTflStatus() 
-  })
+document.addEventListener("DOMContentLoaded", event => {
+  showTflStatus();
+});
